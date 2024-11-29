f9a94d5f079297d1d1756f2cf37b71d40c340322
Gabriel Gambotti <gabrielgambotti2006@gmail.com>
2024-11-29 13:48:35 +0100

    boucle sur les works ajouter

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -3,26 +3,46 @@ const data = {
     categories: []
   }
 
+  function renderFilters(categories) {
+
   window.addEventListener("DOMContentLoaded", async () => {
     data.works = await getWorks();
     // @TODO: Créer une fonction de récupération des catégories.
-    // data.categories = await getCategories();
+    data.categories = await getCategories();
     renderWorks(data.works);
     // Afficher les filtres de catégorie.
-    // renderFilters(data.categories);
+    renderFilters(data.categories);
   })
+}
 
   async function getWorks() {
-    const response= await fetch("http://localhost:5678/api/works");
+    const response = await fetch("http://localhost:5678/api/works");
+    return await response.json();
+  }
+
+  async function getCategories() {
+    const response = await fetch("http://localhost:5678/api/categories");
     return await response.json();
   }
 
   function renderWorks(works) {
     const gallery = document.querySelector(".gallery");
     // @TODO : Boucler sur les works pour les afficher dans la galerie.
+    works.forEach(work => {
+        gallery.innerHTML += `
+        <div class="work">
+            <img src="${work.image}" alt="${work.title}">
+            <h3>${work.title}</h3>
+            <p>${work.category}</p>
+        </div>
+        `
+    });
   }
 
   // @TODO: Créer une fonction de filtrage des works par catégorie.
+
+  const boutonTrier = document.querySelector(".btn-trier");
+
   boutonTrier.addEventListener("click", function() {
     const works = Array.from(data.works);
     works.sort(function(a, b) {
@@ -30,8 +50,5 @@ const data = {
     })
     console.log(works);
 });
+
   // @TODO: Associer un évènement à chaque filtre pour filtrer les works lorsqu'on clique dessus.
-
-
-  // Gestion des fonction
-  const boutonTrier = document.querySelector(".btn-trier");
